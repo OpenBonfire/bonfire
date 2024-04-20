bb85181aa953ee2410c4d42c79eb5f589c4005e2
EricApostal <ehapostal04@gmail.com>
2024-04-20 10:14:03 -0400

    it extends!

--- lib/main.dart
+++ lib/main.dart
@@ -10,66 +10,39 @@ import 'package:bonfire/features/auth/models/auth.dart';
 import 'package:bonfire/shared/repositories/firebridge/auth.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.top]);
   await GetStorage.init();
-  runApp(const ProviderScope(child: MaterialApp(home: AuthWidgetTest())));
+  runApp(const ProviderScope(child: MaterialApp(home: MainWindow())));
 }
 
-class AuthWidgetTest extends ConsumerStatefulWidget {
-  const AuthWidgetTest({Key? key}) : super(key: key);
+class MainWindow extends StatefulWidget {
+  const MainWindow({Key? key}) : super(key: key);
 
   @override
-  _AuthWidgetTestState createState() => _AuthWidgetTestState();
+  State<MainWindow> createState() => _MainWindowState();
 }
 
-class _AuthWidgetTestState extends ConsumerState<AuthWidgetTest> {
+class _MainWindowState extends State<MainWindow> {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      systemNavigationBarColor:
-          Colors.transparent, // This makes navigation bar transparent
-      systemNavigationBarIconBrightness:
-          Brightness.dark, // You can set the icons' color to dark
-      statusBarColor:
-          Colors.transparent, // This makes the status bar transparent
-    ));
-
-    final MediaQueryData queryData = MediaQuery.of(context);
-    final Size viewportSize = queryData.size;
-
-    String buttonText = "Login";
-    var auth = ref.watch(authProvider);
-    auth.when(
-      data: (data) {
-        if (data is AuthSuccess) {
-          buttonText = "Logged in";
-        } else if (data is CaptchaResponse) {
-          buttonText = "Captcha";
-        }
-      },
-      loading: () {
-        buttonText = "Loading";
-      },
-      error: (error, stack) {
-        buttonText = "Error";
-      },
-    );
-
-    return Scaffold(
-      resizeToAvoidBottomInset: false, // To avoid resizing due to keyboard
-      body: Container(
-        padding: EdgeInsets.only(
-            bottom:
-                Platform.isAndroid ? 16.0 : 0), // Adjust padding for Android
-        child: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              var resp = await ref
-                  .read(authProvider.notifier)
-                  .loginWithCredentials("username", "password");
-
-              print(resp);
-            },
-            child: Text(buttonText),
+    // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    //   systemNavigationBarColor: Colors.transparent,
+    //   systemNavigationBarIconBrightness: Brightness.dark,
+    //   statusBarColor: Colors.transparent,
+    // ));
+    return MaterialApp(
+      title: 'Bonfire',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        extendBody: true,
+        body: Container(
+          color: const Color.fromARGB(255, 115, 69, 69),
+          child: const Center(
+            child: Text('Hello World'),
           ),
         ),
       ),
