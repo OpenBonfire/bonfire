8d0680d4a2c19e764f6038d68c4ff95c2c75087d
elyzk <elyzkatz@gmail.com>
2024-05-16 19:57:59 -0400

    Added search button to guild overview and fixed alignment

--- lib/features/guild/views/guild_overview.dart
+++ lib/features/guild/views/guild_overview.dart
@@ -2,7 +2,9 @@ import 'dart:math';
 
 import 'package:bonfire/theme/text_theme.dart';
 import 'package:bonfire/theme/theme.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:bonfire/features/guild/controllers/current_guild.dart';
 
@@ -22,7 +24,6 @@ class _GuildOverviewState extends ConsumerState<GuildOverview> {
     return SizedBox(
         width: double.infinity,
         child: Container(
-          height: 60,
           decoration: BoxDecoration(
               color: Theme.of(context).custom.colorTheme.foreground,
               borderRadius: const BorderRadius.only(
@@ -31,26 +32,65 @@ class _GuildOverviewState extends ConsumerState<GuildOverview> {
                   bottom: BorderSide(
                       color: Theme.of(context).custom.colorTheme.brightestGray,
                       width: 1.0))),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Row(
-              children: [
-                Flexible(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 15.0),
-                    child: Text(
-                      overflow: TextOverflow.ellipsis,
-                      guildTitle,
-                      style: CustomTextTheme().titleSmall.copyWith(
-                            color: Colors.white,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 15.0),
+                        child: Text(
+                          overflow: TextOverflow.ellipsis,
+                          guildTitle,
+                          style: CustomTextTheme().titleSmall.copyWith(
+                                color: Colors.white,
+                              ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Transform.rotate(
+                          angle: pi / 2,
+                          child: const Icon(Icons.expand_less_rounded)),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20, right: 20, top: 8.0, bottom: 8.0),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).custom.colorTheme.brightestGray,
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(100)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.search,
+                            color:
+                                Theme.of(context).custom.colorTheme.textColor1,
                           ),
+                          Text(
+                            "Search",
+                            style: CustomTextTheme().bodyText1,
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                Transform.rotate(
-                    angle: pi / 2, child: const Icon(Icons.expand_less_rounded))
-              ],
-            ),
+              ),
+            ],
           ),
         ));
   }
