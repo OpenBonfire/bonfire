ac095d8a26305d772f986f6bba306262000d1273
EricApostal <ehapostal04@gmail.com>
2024-08-20 08:19:13 -0400

    fixed bad import

--- lib/features/sidebar/components/guild_folder.dart
+++ lib/features/sidebar/components/guild_folder.dart
@@ -7,7 +7,6 @@ import 'package:firebridge/firebridge.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hex/hex.dart';
 
 class GuildFolderWidget extends ConsumerStatefulWidget {
   final GuildFolder guildFolder;
