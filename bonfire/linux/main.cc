c73ed41fd711941c45d61a080f78cb25decae517
EricApostal <eaposta@ncsu.edu>
2024-12-19 09:34:13 -0500

    add CEF to linux main file, add gcc to build

--- bonfire/linux/main.cc
+++ bonfire/linux/main.cc
@@ -1,6 +1,8 @@
 #include "my_application.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
+  initCEFProcesses(argc, argv);
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
