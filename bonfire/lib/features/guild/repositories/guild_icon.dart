2070ecd45189c38dcfe8182b52c955457ed98235
Eric Apostal <ehapostal04@gmail.com>
2025-03-11 18:50:21 -0400

    Improve guild icons not loading / slow load times

--- bonfire/lib/features/guild/repositories/guild_icon.dart
+++ bonfire/lib/features/guild/repositories/guild_icon.dart
@@ -2,44 +2,42 @@ import 'dart:typed_data';
 
 import 'package:bonfire/features/auth/data/repositories/auth.dart';
 import 'package:bonfire/features/auth/data/repositories/discord_auth.dart';
-import 'package:bonfire/features/me/controllers/settings.dart';
-import 'package:collection/collection.dart';
+import 'package:bonfire/features/guild/controllers/guild.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:firebridge/firebridge.dart';
 
 part 'guild_icon.g.dart';
 
-final iconCache = CacheManager(
-  Config(
-    'guild_icons',
-    maxNrOfCacheObjects: 200,
-  ),
-);
+// final iconCache = CacheManager(
+//   Config(
+//     'guild_icons',
+//     maxNrOfCacheObjects: 200,
+//   ),
+// );
 
 @Riverpod(keepAlive: true)
-Future<Uint8List?> guildIcon(GuildIconRef ref, Snowflake guildId) async {
+Future<Uint8List?> guildIcon(Ref ref, Snowflake guildId) async {
   var user = ref.watch(authProvider.notifier).getAuth();
-  if (user is! AuthUser) return Uint8List(0);
+  if (user is! AuthUser) return null;
 
-  Guild guild = ref.watch(guildsStateProvider).valueOrNull!.firstWhereOrNull(
-        (element) => element.id == guildId,
-      )!;
+  Guild guild = ref.watch(guildControllerProvider(guildId))!;
 
   if (guild.icon == null) return null;
-  Uint8List? fromCache =
-      await (await iconCache.getFileFromCache(guild.iconHash!))
-          ?.file
-          .readAsBytes();
+  // Uint8List? fromCache =
+  //     await (await iconCache.getFileFromCache(guild.iconHash!))
+  //         ?.file
+  //         .readAsBytes();
 
-  if (fromCache != null) {
-    return fromCache;
-  }
+  // if (fromCache != null) {
+  //   return fromCache;
+  // }
 
   Uint8List? icon = await guild.icon?.fetch();
-  if (guild.iconHash != null && icon != null) {
-    iconCache.putFile(guild.iconHash!, icon);
-  }
+  // if (guild.iconHash != null && icon != null) {
+  //   iconCache.putFile(guild.iconHash!, icon);
+  // }
 
   return icon;
 }
