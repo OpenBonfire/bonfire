ac93b1da844f920923400944a02a47937c67478a
Eric Apostal <ehapostal04@gmail.com>
2026-01-03 15:34:17 -0500

    some folder work

--- lib/features/guilds/components/sidebar/folder.dart
+++ lib/features/guilds/components/sidebar/folder.dart
@@ -1,4 +1,5 @@
 import 'package:bonfire/features/guilds/components/sidebar/guild_item.dart';
+import 'package:bonfire/features/guilds/components/sidebar/item.dart';
 import 'package:bonfire/shared/components/buttons/no_splash.dart';
 import 'package:firebridge/firebridge.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +19,7 @@ class _GuildFolderItemState extends ConsumerState<GuildFolderItem> {
   bool _expanded = false;
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     // return SidebarItem(
     //   selected: false,
     //   title: "Folder",
@@ -26,49 +28,67 @@ class _GuildFolderItemState extends ConsumerState<GuildFolderItem> {
     if (widget.folder.id == null) {
       return GuildSidebarItem(guildId: widget.folder.guildIds.first);
     }
+    // final items = widget.folder.guildIds
+    //     .take(_expanded ? widget.folder.guildIds.length : 4)
+    //     .map((e) => GuildSidebarItem(guildId: e))
+    //     .toList();
     final items = widget.folder.guildIds
-        .take(_expanded ? widget.folder.guildIds.length : 4)
         .map((e) => GuildSidebarItem(guildId: e))
         .toList();
-    if (!_expanded) {
-      return NoSplashButton(
-        onPressed: () {
-          HapticFeedback.lightImpact();
-          setState(() {
-            _expanded = !_expanded;
-          });
-        },
-        child: AbsorbPointer(
-          child: GridView.count(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            crossAxisCount: 2,
-            children: items.toList(),
-          ),
-        ),
-      );
-    }
-    return Column(
-      children: [
-        NoSplashButton(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 12.0, top: 12, bottom: 12),
-            child: AspectRatio(
-              aspectRatio: 1,
-              child: SizedBox.expand(
-                child: Center(child: Icon(Icons.folder_rounded)),
+    final color = widget.folder.color != null
+        ? Color(
+            int.parse(widget.folder.color.toString(), radix: 10),
+          ).withValues(alpha: 0.4)
+        : theme.colorScheme.primary.withValues(alpha: 0.4);
+
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      decoration: BoxDecoration(color: color),
+      child: Column(
+        children: [
+          if (_expanded) ...[
+            NoSplashButton(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 12.0, top: 12, bottom: 12),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: SizedBox.expand(
+                    child: Center(child: Icon(Icons.folder_rounded)),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                HapticFeedback.lightImpact();
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+            ...items,
+          ] else
+            SidebarItem(
+              title: "Folder",
+              selected: false,
+              onPressed: () {
+                HapticFeedback.lightImpact();
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(color: color),
+                child: AbsorbPointer(
+                  child: GridView.count(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    crossAxisCount: 2,
+                    children: items.toList(),
+                  ),
+                ),
               ),
             ),
-          ),
-          onPressed: () {
-            HapticFeedback.lightImpact();
-            setState(() {
-              _expanded = !_expanded;
-            });
-          },
-        ),
-        ...items,
-      ],
+        ],
+      ),
     );
   }
 }
